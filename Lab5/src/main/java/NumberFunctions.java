060a0c4bdd0a986227c96ee4c7ce7b10646f67fe
Nicholas Bosenbeck <63688856+bosenj@users.noreply.github.com>
2021-04-28 17:41:10 -0400

    Update and rename Lab5.java to NumberFunctions.java
    Functions that will allow you to figure out various information encoded in Java.

--- Lab5/src/main/java/NumberFunctions.java
+++ Lab5/src/main/java/NumberFunctions.java
@@ -2,20 +2,9 @@
 import java.util.Random;
 import java.util.Scanner;
 
+@author soblab
 
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author soblab
- */
-public class Lab5 {
+public class NumberFunctions {
     public static void main(String[] args){
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Enter a non-negative integer: ");
